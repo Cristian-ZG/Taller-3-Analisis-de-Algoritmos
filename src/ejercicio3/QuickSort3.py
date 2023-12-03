ce267846e38a2f34530d7bd5c695987c4f47b39c
Cristian Zuñiga <zuniga.guti.cristian@gmail.com>
2023-12-03 17:09:58 -0500

    Version 3 QS

---
+++ src/ejercicio3/QuickSort3.py
@@ -0,0 +1,26 @@
+# Algoritmo QuickSort version de Hoare.
+class QuickSort3:
+    @staticmethod
+    def divicion(lista, menor, mayor):
+        pivote = lista[menor]
+        izq = menor +1
+        der = mayor
+
+        while True:
+            while izq <= der and lista[izq] <= pivote:
+                izq += 1
+            while izq <= der and lista[der] >= pivote:
+                der -= 1
+            if der < izq:
+                break
+            else:
+                lista[izq], lista[der] = lista[der], lista[izq]
+        lista[menor], lista[der] = lista[der], lista[menor]
+        return der
+
+    @staticmethod
+    def quickSort(lista, menor, mayor):
+        if menor < mayor:
+            pivote = QuickSort3.divicion(lista, menor, mayor)
+            QuickSort3.quickSort(lista, menor, pivote-1)
+            QuickSort3.quickSort(lista, pivote+1, mayor)
